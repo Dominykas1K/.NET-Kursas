32b84965884acfa7dcffa5e2c3b77d6a5b3c1967
Dominykas1K <dominyykas.k@gmail.com>
2024-10-10 22:07:06 +0300

    4 pamoka

--- 4pamoka/Program.cs
+++ 4pamoka/Program.cs
@@ -1,2 +1,156 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int day = 3;
+//switch (day) 
+//{
+//    case 1:
+//        Console.WriteLine("Pirmadienis");
+//        break;
+//    case 2:
+//        Console.WriteLine("antradienis");
+//        break;
+//    case 3:
+//        Console.WriteLine("treciadienis");
+//        break;
+//    default:
+//        Console.WriteLine("neteisinga diena");
+//        break;
+
+//}
+
+//int operation = 1;
+//string result = operation switch
+//{
+//    1 => "Case 1",
+//    2 => "Case 2",
+//    3 => "Case 3",
+//    _ => "Default Case"
+
+//};
+
+//Console.WriteLine(result);
+
+// 1uzduotis
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Console.WriteLine("Irasykite dienos skaiciu");
+
+//int input = Convert.ToInt32(Console.ReadLine());
+
+//switch (input)
+//{
+//    case 1:
+//        Console.WriteLine("Pirmadienis");
+//        break;
+
+//    case 2:
+//        Console.WriteLine("Antradienis");
+//        break;
+
+//    case 3:
+//        Console.WriteLine("Treciadienis");
+//        break;
+//    case 4:
+//        Console.WriteLine("Ketvirtadienis");
+//        break;
+//    case 5:
+//        Console.WriteLine("Penktadienis");
+//        break;
+//    case 6:
+//        Console.WriteLine("Siastadienis");
+//        break;
+//    case 7:
+//        Console.WriteLine("Sekmadienis");
+//        break;
+//    default:
+//        Console.WriteLine("Tokios dienos nera");
+//        break;
+//}
+
+//1 uzduotis
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//Console.WriteLine("Iveskite savo amzius");
+
+//int input = Convert.ToInt32(Console.ReadLine());
+//string result = input switch
+//{
+//    > 7 and < 18 => "moksleivis",
+//    <19 and < 25 => "Studentas",
+//    <26 and < 65  => "Darbuotojas",
+//    <66 and < 100 => "Pensininkas",
+//    _ =>""
+//};
+//Console.WriteLine(result);
+
+//1 uzduotis
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//Console.WriteLine("Irasykite menesio numeri");
+
+//int input = Convert.ToInt32(Console.ReadLine());
+
+//switch (input)
+//{
+//    case 1:
+//        Console.WriteLine("sausis");
+//        break;
+//    case 2:
+//        Console.WriteLine("Vasaris");
+//        break;
+//    case 3: 
+//        Console.WriteLine("Kovas");
+//        break;
+//    case 4: 
+//        Console.WriteLine("Balandis");
+//        break;
+//    case 5: 
+//        Console.WriteLine("Geguze");
+//        break;
+//    case 6: 
+//        Console.WriteLine("Birzelis");
+//        break;
+//    case 7: 
+//        Console.WriteLine("Liepa");
+//        break;
+//    case 8: 
+//        Console.WriteLine("Rugpjutis");
+//        break;
+//    case 9:
+//        Console.WriteLine("Rugsejis");
+//        break;
+//    case 10:
+//        Console.WriteLine("Spalis");
+//        break;
+//    case 11:
+//        Console.WriteLine("Lapkritis");
+//        break;
+//    case 12:
+//        Console.WriteLine("Gruodis");
+//        break;
+//    default:
+//        Console.WriteLine("Tokio menesio nera");
+//        break;
+//}
+
+
+
+//2 uzduotis
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+Console.WriteLine("Iveskite figura");
+Console.WriteLine("1 - Kvadratas");
+Console.WriteLine("2 - Apskritimas");
+Console.WriteLine("3 - Trikampis");
+Console.WriteLine("4 - Staciakampis");
+
+string figura = Console.ReadLine();
+switch (figura)
+{ case "1":
+        Console.WriteLine("Iveskite kvadrato krastines ilgi:");
+        double krastine = Convert.ToDouble
+
+        
+     
+        break;
+
+
+
+
+
+}
