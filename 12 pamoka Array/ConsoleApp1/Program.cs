33065d17970b45f8186519773612454b9443b1b5
Dominykas1K <dominyykas.k@gmail.com>
2024-10-24 22:00:40 +0300

    lessons added

---
+++ 12 pamoka Array/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿//int[] emtyArray = new int[10];
+//int[] arrayWithInitialValues = { 1, 2, 88, 105, 999 };
+
+//Console.WriteLine(emtyArray[5]);
+
+//arrayWithInitialValues[4] = 23;
+
+
+
+//string[] sentences = { "random text", "one one one", "yes no", "sentence" };
+//for (int i = 0; i < sentences.Length; i++)
+//{
+//    Console.WriteLine(sentences[i]);
+//}
+
+//for (int i = 0;i < emtyArray.Length; i++)
+//{
+//    emtyArray[i] = i * i;
+//    Console.WriteLine(emtyArray[1]);
+//}
+
+
+
